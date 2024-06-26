7a5362c8eac2d58630b800c2c398896b7abe3cb4
Vang Huynh <huynh.vang@devexperts.com>
2024-06-26 11:19:12 +0700

    image and seo handle

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,14 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Feed - ${numberOfPosts} posts`,
+    description: "Browse and share amazing posts",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
